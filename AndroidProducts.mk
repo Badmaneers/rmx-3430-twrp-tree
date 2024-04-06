8bdeba42ea157dc4df3bed5329c44f16595fb0c1
ZYLEX <86944731+Badmaneers@users.noreply.github.com>
2024-04-06 06:20:37 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-     $(LOCAL_DIR)/twrp_oplus_mssi_64_cn.mk
+     $(LOCAL_DIR)/aosp_oplus_mssi_64_cn.mk
+
+COMMON_LUNCH_CHOICES := \
+      aosp_oplus_mssi_64_cn-user \
+      aosp_oplus_mssi_64_cn-userdebug \
+      aosp_oplus_mssi_64_cn-eng
